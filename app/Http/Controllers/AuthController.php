cdbdfc3fd48fedc0da15ab3a5fcd0b14af493c1e
Jonatas <jonatasmc04@gmail.com>
2025-05-31 17:36:07 -0300

    Implementação de autenticação com Sanctum

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,53 @@
+<?php 
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|min:3|max:255',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password']),
+        ]);
+
+        $user->wallet()->create(['balance' => 0.00]);
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json(['token' => $token, 'user' => $user], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            /** @var \App\Models\User $user */
+            $token = $user->createToken('auth_token')->plainTextToken;
+            return response()->json(['token' => $token, 'user' => $user], 200);
+        }
+
+        return response()->json(['error' => 'Credenciais inválidas'], 401);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return response()->json(['message' => 'Logout realizado com sucesso'], 200);
+    }
+}
--- app/Models/wallet.php
+++ app/Models/wallet.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class wallet extends Model
+class Wallet extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'balance'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
